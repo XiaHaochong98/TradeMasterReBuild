9ea8a71e5f7c717f56e418ff96eec0c26d6ccd39
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-13 21:42:12 +0800

    toolbox

--- trademaster/environments/algorithmic_trading/environment.py
+++ trademaster/environments/algorithmic_trading/environment.py
@@ -318,8 +318,8 @@ class AlgorithmicTradingEnvironment(Environments):
     #     return df_value
 
     def evaualte(self, df):
-        print(df,df.shape,len(df))
         daily_return = df["daily_return"]
+        print(df, df.shape, len(df),len(daily_return))
         neg_ret_lst = df[df["daily_return"] < 0]["daily_return"]
         tr = df["total assets"].values[-1] / (df["total assets"].values[0] + 1e-10) - 1
         sharpe_ratio = np.mean(daily_return) / (np.std(daily_return) * (len(df) ** 0.5) + 1e-10)
