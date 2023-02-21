a01e0cb3961ef8cb21de83be90a6a6dc19867b1b
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-22 00:05:44 +0800

    toolbox

--- trademaster/evaluation/market_dynamics_labeling/custom.py
+++ trademaster/evaluation/market_dynamics_labeling/custom.py
@@ -2,6 +2,7 @@ from .builder import Market_Dynamics_Model
 
 
 @Market_Dynamics_Model.register_module()
-class Market_dynamics_model():
+class Market_dynamics_model:
     def __init__(self, **kwargs):
-        super(Market_Dynamics_Model, self).__init__()
+        pass
+
