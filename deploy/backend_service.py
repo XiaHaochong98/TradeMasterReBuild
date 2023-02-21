3bee72adc5aebe31760aa4e99a22e27886ea7659
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-21 13:39:45 +0800

    toolbox

--- deploy/backend_service.py
+++ deploy/backend_service.py
@@ -24,6 +24,9 @@ import base64
 
 tz = pytz.timezone('Asia/Shanghai')
 
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, root)
+
 ROOT = str(pathlib.Path(__file__).resolve().parents[1])
 sys.path.append(ROOT)
 
